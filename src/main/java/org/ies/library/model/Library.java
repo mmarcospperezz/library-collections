d1e41ee5a13fb10e7d396182766cc207d101c693
mmarcospperezz <mmarcospperez2204@gmail.com>
2025-03-11 11:13:11 +0100

    library

--- src/main/java/org/ies/library/model/Library.java
+++ src/main/java/org/ies/library/model/Library.java
@@ -3,6 +3,8 @@ package org.ies.library.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeSet;
@@ -15,4 +17,28 @@ public class Library {
     private List<Customer> customers;
     private TreeSet<BookLend> bookLends;
 
+    public List<Book> getBooksByGenre(String genre) {
+        List<Book> result = new ArrayList<>();
+        for (Book book : bookByIsbn.values()) {
+            if (book.getGenres().contains(genre)) {
+                result.add(book);
+            }
+        }
+        return result;
+    }
+
+    public List<Customer> getCustomersbyZipCode (int zipCode){
+        List<Customer> result = new ArrayList<>();
+        for(Customer customer: customers){
+            if(customer.getZipCode()==zipCode){
+                result.add(customer);
+            }
+        }
+        return result;
+    }
+
+    public void createBookLend(String nif, String isbn) {
+
+
+    }
 }
